2cade9c0b3dad1ed27f8d4ffa1bfde8cef097bdd
Lee Rosenberg <laluk321@gmail.com>
2023-07-18 01:54:03 +0300

    add tests for utils

---
+++ src/bytewax_ros/test/test_utils.py
@@ -0,0 +1,11 @@
+import pytest
+
+from bytewax_ros.utils import value_from_obj
+
+
+@pytest.mark.parametrize(
+    "obj, attribute, value",
+    [({"a": {"b": 5}}, "obj['a']['b']", 5), ([1, 2, 3], "obj[2]", 3), (1, "obj", 1)],
+)
+def test_value_from_obj(obj, attribute, value):
+    assert value_from_obj(obj, attribute) == value
