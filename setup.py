f79116bfd462b87763662bb6475e3bae2043e7d9
Lee Rosenberg <laluk321@gmail.com>
2023-07-18 02:08:55 +0300

    update description

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup
 
-package_name = 'bytewax_ros'
+package_name = "bytewax_ros"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.1",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='leeros',
-    maintainer_email='laluk321@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="lee-ros",
+    maintainer_email="laluk321@gmail.com",
+    description="An extention package for the Bytewax data-processing library to allow connection for ROS2",
+    license="",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
