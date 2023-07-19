c399cb0909c6e1505b1cc079b0db488dd063be52
Lee Rosenberg <laluk321@gmail.com>
2023-07-19 23:52:14 +0300

    update to support new utils

--- bytewax_ros/bands/band_detector.py
+++ bytewax_ros/bands/band_detector.py
@@ -1,18 +1,20 @@
 from typing import List, Optional
 
-from bytewax_ros.bands import Band, DefaultBand
-from bytewax_ros.utilities import verify_bands
+from bytewax_ros.bands import Band, DefaultBand, verify_no_intersections
+
 
 class BandDetector:
-    def __init__(self, bands: List[Band], *, default_band: Optional[DefaultBand] = None):
-        if verify_bands(bands):
-            self._bands = bands
-        
+    def __init__(
+        self, bands: List[Band], *, default_band: Optional[DefaultBand] = None
+    ):
+        verify_no_intersections(bands)
+        self._bands = bands
+
         self._default_band = default_band
 
     def detect_band(self, sample: float) -> Optional[Band]:
         bands = [band for band in self._bands if band.check_sample(sample)]
-        
+
         if bands:
             return bands[0]
         if self._default_band is not None:
