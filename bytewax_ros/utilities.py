5368b6b986aa2e758dd6832b691b5bc1481b5b32
Lee Rosenberg <laluk321@gmail.com>
2023-07-19 23:17:59 +0300

    update intersection utils

--- bytewax_ros/utilities.py
+++ bytewax_ros/utilities.py
@@ -27,24 +27,25 @@ def value_from_obj(obj: Any, attribute: str) -> Any:
     return eval(attribute, {}, {"obj": obj})
 
 
-def intersect_bands(bands: List[Band]) -> Tuple[bool, Optional[Band], Optional[Band]]:
-    """Find the first intersecting bands in a list of bands
+def find_intersections(bands: List[Band]) -> List[Tuple[Band, Band]]:
+    """Find intersections in a list of bands
 
     Args:
         bands: list of bands to search in
 
     Returns:
-        A tuple of (bool, band1, band2) indicating if an intersection was found and it's correspondig bands
+        A list of two-tuple containing the intersecting bands
     """
+    intersections = []
     bands.sort(key=lambda band: band.lower_limit)
 
     for i in range(len(bands) - 1):
         if bands[i].intersect(bands[i + 1]):
-            return True, bands[i], bands[i + 1]
-    return False, None, None
+            intersections.append((bands[i], bands[i + 1]))
+    return intersections
 
 
-def verify_bands(bands: List[Band]) -> bool:
+def verify_no_intersections(bands: List[Band]) -> bool:
     """Verify that a given list of bands has no intersections
 
     Args:
@@ -56,9 +57,9 @@ def verify_bands(bands: List[Band]) -> bool:
     Raises:
         `ValueError` if the bands has intersection
     """
-    has_intersection, band1, band2 = intersect_bands(bands)
-    if has_intersection:
-        raise ValueError(f"{band1} has intersection with {band2}")
+    intersections = find_intersections(bands)
+    if intersections:
+        raise ValueError(f"Intersections found at: {intersections}")
     return True
 
 
